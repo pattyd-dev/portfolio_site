9e5b776a922e453d8b2a0e62bf643006999aa258
pattyd-dev <pattydserver@gmail.com>
2026-01-03 13:14:23 -0500

    Added regional domain name output for S3

--- data_stores/outputs.tf
+++ data_stores/outputs.tf
@@ -7,3 +7,8 @@ output "s3_arn" {
   description = "The arn of the site data S3 bucket."
   value       = aws_s3_bucket.site_bucket.arn
 }
+
+output "s3_regional_domain" {
+  description = "Regional domain name of site data S3 bucket."
+  value = aws_s3_bucket.site_bucket.bucket_regional_domain_name
+}
